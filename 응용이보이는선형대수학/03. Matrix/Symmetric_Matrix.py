d7f779ca4acb1acb6d3191674866173d2cd0aa37
jugahy <jugahy1205@gmail.com>
2022-05-28 19:08:51 +0900

    ICanDoIt

---
+++ 응용이보이는선형대수학/03. Matrix/Symmetric_Matrix.py
@@ -0,0 +1,13 @@
+"""
+임의의 정방행렬 A에 대해 A+A transpose는 대칭행렬이고
+A-A transpose는 반대칭행렬이다.
+A = 1/2(A+A transpose) + 1/2(A-A transpose)를 증명해보겠습니다.
+"""
+import numpy as np
+
+A = np.array([[1, 2],
+              [3, 4]])
+
+trans_A = np.transpose(A)
+
+print(1/2(A + trans_A) + 1/2(A-trans_A))
