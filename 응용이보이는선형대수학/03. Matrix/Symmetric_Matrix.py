8d5f173d7f1b25c0d4cf2ba361f4bbb0441e15b3
jugahy <jugahy1205@gmail.com>
2022-05-29 16:07:02 +0900

    대칭행렬과 반대칭핼렬로 정방행렬 분해

--- 응용이보이는선형대수학/03. Matrix/Symmetric_Matrix.py
+++ 응용이보이는선형대수학/03. Matrix/Symmetric_Matrix.py
@@ -1,7 +1,9 @@
 """
 임의의 정방행렬 A에 대해 A+A transpose는 대칭행렬이고
 A-A transpose는 반대칭행렬이다.
-A = 1/2(A+A transpose) + 1/2(A-A transpose)를 증명해보겠습니다.
+정방행렬 A는 대칭행렬과 반대칭행렬로 쪼갤 수 있습니다.
+
+-> A = 1/2(A+A transpose) + 1/2(A-A transpose)를 증명해보겠습니다.
 """
 import numpy as np
 
@@ -10,4 +12,4 @@ A = np.array([[1, 2],
 
 trans_A = np.transpose(A)
 
-print(1/2(A + trans_A) + 1/2(A-trans_A))
+print(1 / 2 * (A + trans_A) + 1 / 2 * (A - trans_A))
